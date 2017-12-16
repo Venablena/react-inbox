7f1017dea5093e7bd9387a4c526e44087de9f090
Lena Venable <venablena@gmail.com>
2017-12-15 17:42:08 -0800

    add toolbar and one message

---
+++ src/components/MessageList.js
@@ -0,0 +1,8 @@
+import React from 'react'
+//import Message from './components/Message.js'
+
+// const MessageList = ({msg}) => (
+//   msg.data.map(el => <Message msg = {el} />)
+// )
+
+export default MessageList

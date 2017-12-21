e49cd24fae3634dedd7d55785211dfdaca4d2f02
Lena Venable <venablena@gmail.com>
2017-12-21 15:23:22 -0800

    API fecth

--- src/App.js
+++ src/App.js
@@ -7,85 +7,31 @@ import './index.css';
 import Toolbar from './components/Toolbar.js'
 import MessageList from './components/MessageList.js'
 
-const data = [
-  {
-    "id": 1,
-    "subject": "You can't input the protocol without calculating the mobile RSS protocol!",
-    "read": false,
-    "starred": true,
-    "labels": ["dev", "personal"]
-  },
-  {
-    "id": 2,
-    "subject": "connecting the system won't do anything, we need to input the mobile AI panel!",
-    "read": false,
-    "starred": false,
-    "selected": true,
-    "labels": []
-  },
-  {
-    "id": 3,
-    "subject": "Use the 1080p HTTP feed, then you can parse the cross-platform hard drive!",
-    "read": false,
-    "starred": true,
-    "labels": ["dev"]
-  },
-  {
-    "id": 4,
-    "subject": "We need to program the primary TCP hard drive!",
-    "read": true,
-    "starred": false,
-    "selected": true,
-    "labels": []
-  },
-  {
-    "id": 5,
-    "subject": "If we override the interface, we can get to the HTTP feed through the virtual EXE interface!",
-    "read": false,
-    "starred": false,
-    "labels": ["personal"]
-  },
-  {
-    "id": 6,
-    "subject": "We need to back up the wireless GB driver!",
-    "read": true,
-    "starred": true,
-    "labels": []
-  },
-  {
-    "id": 7,
-    "subject": "We need to index the mobile PCI bus!",
-    "read": true,
-    "starred": false,
-    "labels": ["dev", "personal"]
-  },
-  {
-    "id": 8,
-    "subject": "If we connect the sensor, we can get to the HDD port through the redundant IB firewall!",
-    "read": true,
-    "starred": true,
-    "labels": []
-  }
-]
+const API = 'http://localhost:8082/api/messages'
 
 class App extends Component {
   constructor(props){
     super(props)
-    data.map(el => el.checked = false)
-    this.state = {selection: data}
+    this.state = {selection: []}
   }
 
   check = (msgId, action) => {
+    //update state
     const posts = this.state.selection
     const match = posts.find(el => el.id === msgId)
     match[action] ? match[action] = false : match[action] = true
     const newSelection = [...posts.slice(0, posts.indexOf(match)), match, ...posts.slice(posts.indexOf(match)+1)]
+
+    //re-render & set state
     this.setState({selection:newSelection})
   }
 
   trash = (msgId) => {
+    //update state
     const posts = Object.assign({}, this.state)
     posts.selection.filter(el => el.checked).forEach(el => posts.selection.splice(posts.selection.indexOf(el),1))
+    //update database
+    //await fetch()
     this.setState(posts)
   }
 
@@ -117,6 +63,28 @@ class App extends Component {
     })
     this.setState(posts)
   }
+
+  async componentDidMount(){
+    const posts = await fetch(API)
+    const response = await posts.json()
+    const data = response._embedded.messages.map(el => {
+      const {id, subject, read, starred, labels} = el
+        return {id, subject, read, starred, labels}
+    })
+    data.map(el => el.checked = false)
+    this.setState({selection: data})
+  }
+
+//same without async, in promise format:
+  // componentDidMount(){
+  //   fetch('http://localhost:8082/api/messages')
+  //     .then((result) => {
+  //       console.log(result)
+  //       result.json().then(response => {
+  //         console.log(response)
+  //       })
+  //     })
+  // }
 
   render() {
     return (
